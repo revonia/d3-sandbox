4518a343a3fe3a7e83d5097b9d6d81b84d69387b
revonia <revonia64@gmail.com>
2019-07-08 18:37:51 +0800

    update "world map labor force total"

--- src/examples/world-map-labor-force-total/world-map-labor-force-total.js
+++ src/examples/world-map-labor-force-total/world-map-labor-force-total.js
@@ -4,10 +4,13 @@ const svg = d3.select('body')
   .attr('height', '500px')
 
 const rect = svg.node().getBoundingClientRect()
-const padding = 20
+const padding = 60
 const size = [rect.width - padding * 2, rect.height - padding * 2]
 const radius = d3.min(size) / 2
 const range = [2000, 2018]
+const rotateSpeed = 0.02
+const topN = 40
+const donutWidth = 15
 
 const projection = d3
   .geoOrthographic()
@@ -15,31 +18,128 @@ const projection = d3
   .translate([rect.width / 2, padding + radius])
   .precision(0)
 
+const center = projection(projection.center())
 const path = d3.geoPath(projection)
 
-async function loadMap () {
+;(async function run () {
+  const { land, countries, stat } = await loadData()
+
+  const scale = d3.scaleLinear()
+    .domain([stat.min, stat.max])
+
+  const colors = scale.copy().range(['#b3e2d7', '#00431b'])
+
+  const earth = svg.append('g')
+  const updateEarth = drawEarth(earth, land, countries, colors)
+
+  const donut = svg.append('g')
+  const updateDonut = drawDonut(donut, stat, colors, radius + donutWidth, donutWidth)
+
+  const valueAxis = svg.append('g')
+  drawValueAxis(valueAxis, scale)
+
+  const yearAxis = svg.append('g')
+  const updateYearAxis = drawYearAxis(yearAxis)
+
+  d3.timer(function (elapsed) {
+    const lambda = (rotateSpeed * elapsed) % 360
+
+    const year = range[0] + parseInt(elapsed / 3 / 1000) % (range[1] - range[0] + 1)
+    projection.rotate([lambda, 0, 0])
+    updateEarth(year)
+    updateDonut(year)
+    updateYearAxis(year)
+  })
+})()
+
+function skipLine (text, n) {
+  let pos = -1
+  while (n > 0) {
+    pos = text.indexOf('\n', pos + 1)
+    n--
+  }
+  return text.substring(pos + 1)
+}
+
+function byValueDesc (a, b) {
+  return b.value - a.value
+}
+
+function calculate (acc, code, row) {
+  const ret = []
+  for (let year = range[0]; year <= range[1]; year++) {
+    const thisYear = parseInt(row[year]) || 0
+    ret[year] = thisYear
+    let stat
+    if (!acc[year]) {
+      stat = acc[year] = {
+        year,
+        total: 0,
+        max: 0,
+        min: Infinity,
+        top: []
+      }
+    } else {
+      stat = acc[year]
+    }
+    if (thisYear !== 0) {
+      stat.total += thisYear
+      if (thisYear > stat.max) {
+        stat.max = thisYear
+      }
+
+      if (thisYear < stat.min) {
+        stat.min = thisYear
+      }
+
+      if (stat.top.length === 0) {
+        stat.top.push({ code, value: thisYear })
+      } else {
+        if (thisYear > stat.top[stat.top.length - 1].value) {
+          stat.top.push({ code, value: thisYear })
+          stat.top.sort(byValueDesc)
+          if (stat.top.length > topN) {
+            stat.top.pop()
+          }
+        }
+      }
+    }
+  }
+  return ret
+}
+
+async function loadData () {
   const world = await d3.json($sandbox.getResource('world-atlas@1/110m.json'))
   const land = topojson.feature(world, world.objects.land)
   const countries = topojson.feature(world, world.objects.countries).features
 
-  const csv = '/resources/dataset/world-bank/labor-force-total-2018/API_SL.TLF.TOTL.IN_DS2_en_csv_v2_7748.csv'
+  const info = new Map()
+  await d3.tsv(
+    $sandbox.getResource('world-atlas@1/110m.tsv'),
+    ({ iso_n3: id, iso_a3: code, name }) => {
+      const data = { id, code, name }
+      info.set(id, data)
+      info.set(code, data)
+    }
+  )
 
+  const stat = []
+
+  const csv = '/resources/dataset/world-bank/labor-force-total-2018/API_SL.TLF.TOTL.IN_DS2_en_csv_v2_7748.csv'
   const data = new Map()
   d3.csvParse(
     skipLine(await d3.text($sandbox.withBase(csv)), 4),
     row => {
-      data.set(row['Country Code'], splice(row))
+      const code = row['Country Code']
+      if (info.has(code)) {
+        data.set(code, calculate(stat, code, row))
+      }
     }
   )
 
-  const info = new Map()
-
-  await d3.tsv(
-    $sandbox.getResource('world-atlas@1/110m.tsv'),
-    ({ iso_n3: id, iso_a3: code, name }) => {
-      info.set(id, { id, code, name })
-    }
-  )
+  const sliced = stat.slice(range[0], range[1])
+  stat.min = d3.min(sliced, data => data.min)
+  stat.max = d3.max(sliced, data => data.max)
 
   countries.forEach(d => {
     d.info = info.get(d.id)
@@ -47,13 +147,11 @@ async function loadMap () {
     d.data = data.get(d.info.code)
   })
 
-  return { land, countries }
+  return { land, countries, stat, info }
 }
 
-function drawEarth (g, land, countries) {
-  const landGroup = g.append('g')
-
-  const center = projection(projection.center())
+function drawEarth (root, land, countries, colors) {
+  const landGroup = root.append('g')
 
   landGroup.append('g')
     .classed('sea', true)
@@ -63,9 +161,9 @@ function drawEarth (g, land, countries) {
     .attr('cy', center[1])
     .style('fill', '#aaddff')
 
-  const countriesGroup = g.append('g')
+  const countriesGroup = root.append('g')
 
-  function update () {
+  function update (year) {
     landGroup
       .select('g.land')
       .selectAll('path')
@@ -80,13 +178,13 @@ function drawEarth (g, land, countries) {
       .join('g')
 
     groups.selectAll('path')
-      .data(d => [{ path: path(d), ...d.info }])
+      .data(d => [{ path: path(d), ...d.info, data: d.data }])
       .join('path')
       .attr('d', d => d.path)
       .attr('data-name', d => d.name)
       .attr('data-id', d => d.id)
       .attr('data-code', d => d.code)
-      .style('fill', '#e9ebdf')
+      .style('fill', d => colors(d.data && d.data[year] ? d.data[year] : 0))
       .style('stroke', '#fff')
       .style('stroke-width', 1)
 
@@ -99,38 +197,75 @@ function drawEarth (g, land, countries) {
     //   .text(d => d.id)
   }
 
-  update()
+  update(range[0])
   return update
 }
 
-async function load () {
-  const { land, countries } = await loadMap()
-  const earth = svg.append('g')
+function drawDonut (root, stat, colors, innerRadius, width) {
+  const arc = d3.arc()
+  const pie = d3.pie()
+    .value(d => d.value)
+    .padAngle(Math.PI * 2 / 360)
+    .sort((a, b) => {
+      if (a.code === 'OTH') {
+        return 1
+      }
+      if (b.code === 'OTH') {
+        return -1
+      }
+      return b.value - a.value
+    })
 
-  const update = drawEarth(earth, land, countries)
+  root.attr('transform', `translate(${center[0]}, ${center[1]})`)
 
-  d3.timer(function (elapsed) {
-    const lambda = (0.01 * elapsed) % 360
-    projection.rotate([lambda, 0, 0])
-    update()
-  })
+  function update (year) {
+    const data = pie(stat[year].top)
+
+    root.selectAll('path')
+      .data(data)
+      .join('path')
+      .attr('d', d => arc({
+        ...d,
+        innerRadius: innerRadius,
+        outerRadius: innerRadius + width
+      }))
+      .attr('data-code', d => d.data.code)
+      .attr('data-value', d => d.value)
+      .style('fill', d => colors(d.value))
+  }
+
+  update(range[0])
+  return update
 }
 
-load()
-
-function skipLine (text, n) {
-  let pos = -1
-  while (n > 0) {
-    pos = text.indexOf('\n', pos + 1)
-    n--
+function drawValueAxis (root, scale) {
+  const height = 440
+  const ticksCount = 20
+  const axis = d3.axisRight(scale.copy().range([0, height])).ticks(ticksCount)
+  const position = {
+    x: center[0] + radius + 2 * donutWidth + padding / 2,
+    y: padding / 2
   }
-  return text.substring(pos + 1)
+
+  root.attr('transform', `translate(${position.x}, ${position.y})`)
+
+  root.append('g')
+    .call(axis)
+
+  // root.append('g')
+  //   .selectAll('rect')
+  //   .data(d3.range(0, height, height / ticksCount))
+  //   .join('rect')
+  //   .attr('width', 30)
+  //   .attr('height', height / ticksCount)
+  //   .attr('x', position.x)
+  //   .attr('y', d => d)
+  //   .attr('fill', 'red')
 }
 
-function splice (data) {
-  const ret = []
-  for (let i = range[0]; i <= range[1]; i++) {
-    ret[i] = data[i]
+function drawYearAxis () {
+  function update (year) {
+
   }
-  return ret
+  return update
 }
